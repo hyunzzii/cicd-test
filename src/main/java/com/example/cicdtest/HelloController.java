a6a5ce8d9248b5d9ebbe72b39d40a8be1421092d
hyunzzii <yhynym@gmail.com>
2025-01-07 22:04:51 +0900

    환경변수 적용 테스트

--- src/main/java/com/example/cicdtest/HelloController.java
+++ src/main/java/com/example/cicdtest/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.cicdtest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${dessert}")
+    private String dessert;
 
     @GetMapping
-    public String getHello(){
+    public String getHello() {
         return "hello";
+    }
+
+    @GetMapping("/dessert")
+    public String getDessert() {
+        return dessert;
     }
 }
